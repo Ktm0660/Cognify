bbaf060448c84cdaadc7814ec4dbf5953b711feb
Ktm0660 <jndance06@gmail.com>
2025-09-15 22:09:56 -0600

    test: improve react-router mock

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('react-router-dom');
+
+test('renders without crashing', () => {
+  const { container } = render(<App />);
+  expect(container.firstChild).toBeTruthy();
 });
